50d17ca0fc04e846f415d561b2c0d36bb76d12dd
ChrisRed20 <crisarman010@gmail.com>
2025-02-27 10:55:52 -0600

    Add Firebase configuration and implement HTTP client for API data fetching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,49 @@
-import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Practica_1';
-}
+  posts: any[] = [];
+  fromCache = false;
 
-Notification.requestPermission().then(permission => {
-  if(permission === 'granted') {
-    console.log('Permiso para notificaciones concedido');
-    alert('Ahora las notificaciones estan activadas');
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    // Solicitar permiso para notificaciones
+    Notification.requestPermission().then(permission => {
+      if (permission === 'granted') {
+        console.log('Permiso para notificaciones concedido');
+        alert('Ahora las notificaciones están activadas');
+      }
+    });
+
+    // Obtener datos de la API
+    this.fetchPosts();
   }
-});
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(response => response.json())
-  .then(data => {
-    console.log('Datos obtenidos:', data);
-    localStorage.setItem('posts', JSON.stringify(data));
-  })
-  .catch(() => {
-    console.log('Modo offline: cargando desde caché');
-    const cachedData = localStorage.getItem('posts');
-    if (cachedData) {
-      console.log('Datos en caché:', JSON.parse(cachedData));
-    }
-  });
+  fetchPosts() {
+    this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe(
+      (data: any) => {
+        this.posts = data.slice(0, 10);
+        localStorage.setItem('posts', JSON.stringify(this.posts));
+      },
+      () => {
+        console.log('Modo offline: cargando desde caché');
+        const cachedData = localStorage.getItem('posts');
+        if (cachedData) {
+          this.posts = JSON.parse(cachedData);
+          this.fromCache = true;
+        }
+      }
+    );
+  }
+}
